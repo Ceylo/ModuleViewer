5e228a86d94ac41d39104e8dc2db09fa8b8a9446
Ceylo <lucas.soltic@orange.fr>
2018-08-11 12:37:41 +0200

    Initial commit

---
+++ ModuleViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModuleViewer
+//
+//  Created by Ceylo on 11/08/2018.
+//  Copyright © 2018 Yalir. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
